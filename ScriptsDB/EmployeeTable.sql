d50a7e6b1f9b678a58b6cb083e939b6459aacb42
Emilio Barrera <barrera_emilio@yahoo.es>
2024-10-13 21:29:22 +0100

    Create EmployeeTable.sql

---
+++ ScriptsDB/EmployeeTable.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Employee 
+(
+    Employee_Id number(10) not null,
+    Name VARCHAR2(50) not null,
+    Last_Name VARCHAR2(50) not null,
+    Document_Number VARCHAR2(10) not null,
+    Marital_Status VARCHAR2(1) not null,
+    Gender VARCHAR2(1) not null,
+    Age number(3) not null
+        
+);
+
+ALTER TABLE Employee ADD CONSTRAINT Employee_Id_PK PRIMARY KEY(Employee_Id);
+
+SELECT * FROM employee;
