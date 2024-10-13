62be1b379afdffc30c41f6bc5b2b93875e01381a
Emilio Barrera <barrera_emilio@yahoo.es>
2024-10-13 23:47:10 +0100

    class EmployeeDAO and EmployeeBO Part 1,. Form Employee Done

---
+++ Employee/src/com/employee/uk/bo/EmployeeBO.java
@@ -0,0 +1,41 @@
+package com.employee.uk.bo;
+
+import com.employee.uk.entity.Employee;
+import java.sql.Connection;
+
+/**
+ *
+ * @author Emilio
+ */
+public class EmployeeBO
+{
+    private String message =  "";
+    
+    public  String AddEmployee( Employee employee)
+    {
+               
+        return message;
+    }
+    
+     public  String EditEmployee( Employee employee)
+    {
+        
+        
+        return message;
+    }
+     
+      public  String DeleteEmployee( int id)
+    {
+        
+        
+        return message;
+    }
+      
+       public  void EmployeeList()
+    {
+        
+        
+        
+    }
+    
+}
---
+++ Employee/src/com/employee/uk/dao/EmployeeDAO.java
@@ -0,0 +1,42 @@
+package com.employee.uk.dao;
+
+import com.employee.uk.entity.Employee;
+import java.sql.Connection;
+
+/**
+ *
+ * @author Emilio
+ */
+public class EmployeeDAO
+{
+    
+    private String message = null;
+    
+    public  String AddEmployee( Connection connection , Employee employee)
+    {
+               
+        return message;
+    }
+    
+     public  String EditEmployee(Connection connection , Employee employee)
+    {
+        
+        
+        return message;
+    }
+     
+      public  String DeleteEmployee(Connection connection , int id)
+    {
+        
+        
+        return message;
+    }
+      
+       public  void EmployeeList()
+    {
+        
+        
+        
+    }
+    
+}
